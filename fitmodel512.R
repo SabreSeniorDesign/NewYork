f7ba4ab310de037b899e9677f20a051c6a969567
olivia-buerkle <obuerkle@smu.edu>
2017-05-14 14:04:33 -0500

    I'm not sure what i did here

--- fitmodel512.R
+++ fitmodel512.R
@@ -72,8 +72,8 @@ data = fread ("/users/shelb/Documents/Hotel_shop_amenities_full.csv", header = T
 setnames(data, col.names[col.classes != "NULL"])
 
 #olivia
-#data = fread ("Desktop/Hotel CCM/data/Hotel_shop_amenities_full.csv", header = TRUE, nrows = -1, stringsAsFactors = F,colClasses = col.classes)
-#setnames(data, col.names[col.classes != "NULL"])
+data = fread ("Desktop/Hotel CCM/data/Hotel_shop_amenities_full1.csv", header = TRUE, nrows = -1, stringsAsFactors = F,colClasses = col.classes)
+setnames(data, col.names[col.classes != "NULL"])
 
 #before we do this lets make sure to remove the columns we dont need 
 #removing these columns: num_prop_display, total_prop_display, pref_type
@@ -131,14 +131,21 @@ data$minRate[is.na(data$minRate) & data$rating == "5 CROWN"] <- 435.5
 
 data$rating <- as.factor(data$rating)
 summary(data$rating)
+addNA(data$rating)
+
 #210 hotels do not have rating info 
 #need to check if any of these were booked
-data$rating[is.na(data$rating)] <- 'unknown'
+data$rating[is.na(data$rating)] <- "unknown"
 
+#olivia only 
+#install.packages("forcats")
+#library(forcats)
+#data$rating <- fct_explicit_na(data$rating)
 
 
 #how many are we losing?
 complete.cases(data)
+sum(complete.cases(data))
 #72 lost because the session did not have the hotel rating on it
 #7206/7278=  99% data is retained... good to go
 data = data[complete.cases(data)]
@@ -152,28 +159,32 @@ data = data[, shopDate := as.Date(shopDate, format = "%m/%d/%y")]
 
 ### not necessary 
 ### so we can fit Cox proportional hazards model as CCM
-data[, time:=ifelse(booked==1,1,2)]
-data[, status:=as.numeric(time==1)] #0:censored, 1:recurrence
+#data[, time:=ifelse(booked==1,1,2)]
+#data[, status:=as.numeric(time==1)] #0:censored, 1:recurrence
 
 ### Number of amenities
-data[, amenities:=wifi+pool+shuttle+fitness+breakfast+restaurant+parking]
-data[, stars:=ifelse(rating==".", 0, as.numeric(rating))]
+#data[, amenities:=wifi+pool+shuttle+fitness+breakfast+restaurant+parking]
+#data[, stars:=ifelse(rating==".", 0, as.numeric(rating))]
 
 
 ### Sort
 setkey(data, shopDate, NumericID, propID)
 
 ### Unique dates
-dates = sort(data[, unique(shopDate)])
+#what does this mean, do we anticipate duplicates
+#dates = sort(data[, unique(shopDate)])
 
-### Select test and validation data
-insample_rate = 0.7
-data_train = data[shopDate <= dates[round(insample_rate*length(dates))]]
-data_valid = data[shopDate > dates[round(insample_rate*length(dates))]]
-#rm(data)
-gc()
+#average price of each session
+avgPrice_perSession <- aggregate (minRate ~ transID,
+                                  data = cluster2,
+                                  mean)
 
+# Now have average price of ALL properties looked at in a session
+names (avgPrice_perSession) <- c ("NumericID", "avgPrice_allProps")
 
+#merge with data set
+data <- merge (data, avgPrice_perSession,
+                   by = "transID")
 
 
 
